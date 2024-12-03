4f25497769cd33e78a661ac86af6e0f51b448878
Guilherme Lucas <127703012+guilerm3@users.noreply.github.com>
2024-12-03 12:54:38 -0300

    Feat material-ui-inset

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,31 +1,21 @@
-import styled from 'styled-components'
-import { Box, Container, Grid2 } from '@mui/material' 
-
-const LoginArea = styled.div`
-    background: #666;
-`
-const LoginImage = styled.div`
-  background-image: url(/login-image.svg);
-  background-size: cover;
-  height: 100vh;
-  width: 50vw;
-`
+import { BannerImage } from '@/components'
+import { Box, Container, Grid } from '@mui/material' 
 
 function Login() {
   return (
     <>
     <Box>
-      <Grid2 container>
-        <Grid2 item xs={12} sm={6} sx={{alignItems: 'center', display: 'flex', height:'100vh'}}>
+      <Grid container>
+        <Grid item xs={12} sm={6} sx={{alignItems: 'center', display: 'flex', height:'100vh'}}>
           <Container maxWidth="sm">
-            <LoginArea>Login</LoginArea>
+            <h1>LOGIN</h1>
           </Container>
-        </Grid2>
+        </Grid>
 
-        <Grid2 item sm={6} sx={{display: {sx: 'none', sm: 'block'}}}>
-            <LoginImage/>
-        </Grid2>
-      </Grid2>
+        <Grid item sm={6} sx={{display: {sx: 'none', sm: 'block'}}}>
+            <BannerImage/>
+        </Grid>
+      </Grid>
     </Box>
       
     </>
